26de92ea9db692d696bfa5ceea522cc455652067
ivan patraoav <youcefimohamed1234@gmail.com>
2025-04-18 16:05:04 +0100

    Update cesar.py

--- cesar.py
+++ cesar.py
@@ -2,6 +2,6 @@ def cesar_decrypt(ciphertext, shift):
     decrypted = ''.join(chr((ord(c) - 97 - shift) % 26 + 97) for c in ciphertext)
     return decrypted
 
-ciphertext = "khoor"  # Exemple de texte chiffré avec un décalage de 3 (Hello)
+ciphertext = "khoor"  # Exemple de text chiffré avec un décalage de 3 (Hello)
 shift = 3
 print(f'Decrypted message: {cesar_decrypt(ciphertext, shift)}')
